345b3e7e28e0f082442af5d29e7a5bba9252517e
jiangzhenxing <107x@163.com>
2016-08-17 08:45:32 +0800

    ch02

--- scripts/ch02.scala
+++ scripts/ch02.scala
@@ -1,4 +1,5 @@
 :load scripts/NAStatCounter.scala
+import org.apache.spark.rdd.RDD
 
 case class MatchData(id1: Int, id2: Int, scores: Array[Double], matched: Boolean)
 
@@ -10,10 +11,37 @@ def parse(line: String): MatchData = {
 			  matched = values(11).toBoolean)
 }
 
+object StatUtils extends Serializable {
+	implicit class Stat(rdd: RDD[Array[Double]]) extends Serializable {
+		def arrstats = rdd.map(_.map(NAStatCounter(_))).mapPartitions(iter => Iterator(iter.reduce(_.zip(_).map{case (a,b) => a.merge(b)}))).reduce(_.zip(_).map{case (a,b) => a.merge(b)})
+	}
+}
+
+import StatUtils._
+
 val text = sc.textFile("data/linkage/")
 
 val parsed = text.filter(!_.startsWith("\"id_1\"")).map(parse)
 
-val stats = parsed.map(_.scores).map(_.map(NAStatCounter(_))).mapPartitions(iter => Iterator(iter.reduce(_.zip(_).map{case (a,b) => a.merge(b)}))).reduce(_.zip(_).map{case (a,b) => a.merge(b)})
+// val stats = parsed.map(_.scores).map(_.map(NAStatCounter(_))).mapPartitions(iter => Iterator(iter.reduce(_.zip(_).map{case (a,b) => a.merge(b)}))).reduce(_.zip(_).map{case (a,b) => a.merge(b)})
 
-stats.foreach(println)
+// stats.foreach(println)
+
+val statsm = parsed.filter(_.matched).map(_.scores).arrstats
+val statsn = parsed.filter(!_.matched).map(_.scores).arrstats
+
+println("-- matched stats --------------------")
+statsm.foreach(println)
+println("-- not matched stats -----------------")
+statsn.foreach(println)
+println("-- differences -----------------------")
+statsm.zip(statsn).map{case (x,y) => (x.missing + y.missing, x.stat.mean - y.stat.mean)}.foreach(println)
+
+def nan(d: Double) = if (d == Double.NaN) 0 else d
+val ct = parsed.map(data => (Array(2,5,6,7,8).map(data.scores(_)).map(nan).sum, data))
+
+ct.filter(_._1 > 4.0).map(_._2.matched).countByValue
+// ~: true -> 19868, false -> 376
+ct.filter(_._1 > 3.0).map(_._2.matched).countByValue
+// ~: res12: scala.collection.Map[Boolean,Long] = Map(true -> 20862, false -> 223430)
+
