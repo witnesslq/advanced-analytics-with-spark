031e3785e6f3e959ab81a543524e65bb5c354df0
jiangzhenxing <107x@163.com>
2016-08-29 09:06:50 +0800

    add

---
+++ scripts/ch04.scala
@@ -0,0 +1,31 @@
+:load scripts/utils.scala
+import org.apache.spark.mllib.linalg._
+import org.apache.spark.mllib.regression._
+
+val rawData = sc.textFile("data/covtype.data")
+
+val data = (rawData.map(_.split(",").toDouble)
+				   .map(values => LabeledPoint(values.last-1,Vectors.dense(values.init))))
+
+val Array(trainData,cvData,testData) = 
+		data.randomSplit(Array(0.8,0.1,0.1))
+
+import org.apache.spark.mllib.evaluation._
+import org.apache.spark.mllib.tree._
+import org.apache.spark.mllib.tree.model._
+import org.apache.spark.rdd._
+
+def getMetrics(model: DecisionTreeModel, data: RDD[LabeledPoint]) = {
+	val predictionAndLabels = data.map(example => 
+		(model.predict(example.features), example.label))
+	new MulticlassMetrics(predictionAndLabels)
+}
+
+val model = DecisionTree.trainClassifier(trainData, 7, Map[Int,Int](), "gini", 4, 100)
+
+val metrics = getMetrics(model, cvData)
+
+metrics.confusionMatrix
+println("precision is: " + metrics.precision)
+
+
--- scripts/utils.scala
+++ scripts/utils.scala
@@ -1,11 +1,20 @@
-def some[T](exp: => T) = {
+def some[T](exp: => T) = 
     try {Some(exp)} catch {case e:Exception => None}
-}
 
 object StringUtils extends Serializable {
     implicit class util(s: String) extends Serializable {
-    	def toInteger  = Integer.parseInt(s.trim)
+    	def trimInt  = Integer.parseInt(s.trim)
 	}   
 }
 
+object ArrayUtils extends Serializable {
+    implicit class Util(arr: Array[String]) extends Serializable {
+        def toInt  = arr.map(_.trim.toInt)
+		def toDouble = arr.map(_.trim.toDouble)
+    }
+}
+
+
 import StringUtils._
+import ArrayUtils._
+
